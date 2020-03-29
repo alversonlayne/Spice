fbbae112444ec1e32f26ac7f9344dd6f040ba9ef
Alverson Layne <alverson.layne2@gmail.com>
2020-03-29 17:53:17 -0400

    Completed 'Category Delete' action method and view

--- Areas/Admin/Controllers/CategoryController.cs
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -89,5 +89,21 @@ namespace Spice.Areas.Admin.Controllers
             }
             return View(category);
         }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            var category = await _db.Category.FindAsync(id);
+
+            if(category == null)
+            {
+                return View();
+            }
+            _db.Category.Remove(category);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
