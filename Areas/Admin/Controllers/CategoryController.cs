605a6c6ade54d9e6c0fbd2005be2143dc63c2886
Alverson Layne <alverson.layne2@gmail.com>
2020-04-01 19:23:15 -0400

    Created Details view with "_DetailsAndBackToList" partial view for buttons. Created Details "GET" action method in CategoryController

--- Areas/Admin/Controllers/CategoryController.cs
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -105,5 +105,21 @@ namespace Spice.Areas.Admin.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        //GET - DETAILS
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _db.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
     }
 }
